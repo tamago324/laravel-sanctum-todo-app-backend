31304585978d4763a19313428acf4883535d6451
tamago324 <tamago_pad@yahoo.co.jp>
2021-11-23 15:39:11 +0900

    POST /auth/login に guest ミドルウェアをつけた
    また、/dashboard がログイン後に表示される想定のパスとした

--- routes/auth.php
+++ routes/auth.php
@@ -4,5 +4,6 @@ use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(function () {
-    Route::post('/login', [LoginController::class, 'login']);
+    Route::post('/login', [LoginController::class, 'login'])
+        ->middleware('guest');
 });
